1509f0feb28c3437ce55ba7136a7577b100152b6
Corrhae <corraefrazier@gmail.com>
2026-05-20 16:24:25 -0500

    Imported the distance and address data

--- Package.py
+++ Package.py
@@ -4,6 +4,7 @@ class DeliveryStatus(Enum):
     AT_THE_HUB = 1
     EN_ROUTE = 2
     DELIVERED = 3
+    DELAYED = 4
 
 class Package:
     def __init__ (self, package_id,address, city, state, zip_code, delivery_deadline, package_weight, special_notes, loading_time,time_delivered,delivery_status):
@@ -15,9 +16,11 @@ class Package:
         self.delivery_deadline = delivery_deadline;
         self.package_weight = package_weight;
         self.special_notes = special_notes;
-        self.loading_time = loading_time;
-        self.delivery_status = DeliveryStatus.AT_THE_HUB;
+        self.required_truck = None;
+        self.delivery_status = None
+        self.departure_time = None;
         self.time_delivered = time_delivered;
+
 
     # print package object in human readable form 2. fix the typoin the return statement and have each attriute on its own line.
     def __str__ (self):
@@ -33,9 +36,7 @@ class Package:
 
 
 
-    # create a method to update address errors at the stated time=
 
 
-    #create a method to enure certain packages are on their required trucks
 
 
