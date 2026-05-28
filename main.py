6f6fe504981ae8c9f4cc8ed6f13aceeff16a1005
Corrhae <corraefrazier@gmail.com>
2026-05-28 18:07:19 -0500

    Added a distance function to retrieve the distance between two addresses

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
+import datetime
 
 from Driver import Driver
 from Package import Package
+from Package import DeliveryStatus
 from Package import DeliveryStatus
 from Trucks import Truck
 from HashTable import HashTable
@@ -9,20 +11,25 @@ from datetime import time, timedelta
 
 
 #initialize the Hash Table
-package_hash_table = HashTable()
+package_database= HashTable()
+
 
 # Initializes the Truck objects and Drivers for their route
 driver_1 = Driver(1)
 driver_2 = Driver(2)
 
-truck_1 = Truck(1, driver_1, [], )
-truck_2 = Truck()
-truck_3 = Truck()
+truck_1 = Truck(1, driver_1, datetime.timedelta(hours=8), None )
+truck_2 = Truck(2, driver_2, datetime.timedelta(hours=9, minutes = 5), None )
+truck_3 = Truck(3, None, [])
+
+#trucks with special notes
+packages_with_truck_restriction = [3,36,38]
+grouped_packages = [13,14,15,16,19,20]
+delayed_packages = [6,9,25,28,32]
 
 truck_fleet = [truck_1,truck_2, truck_3]
 
-currentTime = time(8,0,0)
-print(currentTime.hour, currentTime.minute, currentTime.second)
+
 
 
 # import package data and initialize hash table
@@ -75,7 +82,7 @@ def import_addresses():
     # Return the address list
     return address_list
 
-#
+#import distance data
 def initialize_distances():
     # initialize a 2D array to hold the distances between the 27 addresses
     address_distances = [[0.0 for x in range(27)] for y in range(27)]
@@ -102,12 +109,84 @@ def initialize_distances():
 
         return address_distances
 
+# helper function to get the distance between stops
+def get_distance(point_a,point_b):
+    #initilaize the address list and distance matrix
+    address_list = import_addresses()
+    distances = initialize_distances()
 
-def load_packages(hash_table, truck_fleet):
+    # find the index of the given address on the address list
+    index_1 = address_list.index(point_a)
+    index_2 = address_list.index(point_b)
+
+    # find the intersection between the two points and return the distance
+    distance = distances[index_1][index_2]
+    return distance
+
+
+# assigns packages to trucks
+def load_packages(hash_table, truck1, truck2, truck3):
+# create a loop that goes through the truck list and takes that as an attribute
+    for package_id in range(len(hash_table)):
+        package = hash_table.get(package_id)
+
+        # if the truck has a truck requirement it is loaded to that truck
+        if package_id in packages_with_truck_restriction:
+            truck_2.packages.append(package)
+            package.status = DeliveryStatus(1)
+
+        # Checks if the packages need to be grouped
+        elif package_id in grouped_packages:
+            truck_1.packages.append(package)
+            package.status = DeliveryStatus(1)
+
+        # Checks for package delay
+        elif package_id in delayed_packages:
+            truck_2.packages.append(package)
+            package.status = DeliveryStatus(4)
+
+    # loads the remaining packages
+    for  package_id in range(len(hash_table)):
+        package = hash_table.get(package_id)
+
+        # Checks if package is in either Truck 1 or 2 and skips if found
+        if package in truck_1.packages or package in truck_2.packages:
+            continue
+
+        if "10:30" in package.deadline:
+            if truck_1.packages.length < truck_1.capacity:
+                truck_1.packages.append(package)
+
+            elif truck_2.packages.length < truck_2.capacity:
+                truck_2.packages.append(package)
+
+            elif truck_3.packages.length < truck_3.capacity:
+                truck_3.packages.append(package)
+        else:
+
+            if truck_1.packages.length < truck_1.capacity:
+                truck_1.packages.append(package)
+
+            elif truck_2.packages.length < truck_2.capacity:
+                truck_2.packages.append(package)
+
+            elif truck_3.packages.length < truck_3.capacity:
+                 truck_3.packages.append(package)
 
 
 
 ##### Delivery (Nearest Neighbor) Algorithm #######
+
+def route_and_deliver(hash_table, truck):
+    shortest_distance = float("inf")
+
+    for package in truck:
+        distance = truck.location
+
+
+
+
+
 
 
 
